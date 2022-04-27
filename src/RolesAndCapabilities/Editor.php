7d74830508bf70074363ed6215635945bc04c242
Fred Bradley <code@fredbradley.co.uk>
2022-04-27 09:46:50 +0100

    update: give editor permissions to edit vacancies

--- src/RolesAndCapabilities/Editor.php
+++ src/RolesAndCapabilities/Editor.php
@@ -4,28 +4,25 @@
 	namespace FredBradley\CranleighWPAdmin\RolesAndCapabilities;
 
 
-	class Editor extends BaseCapabilities
-	{
+	class Editor extends BaseCapabilities {
 
-		public function handle(): void
-		{
-			$editor = get_role("editor");
+		public function handle(): void {
+			$editor = get_role( "editor" );
 			try {
-				$this->givePermissionsToPostTypes(['faqs'], $editor);
-			} catch (\TypeError $exception) {
-				error_log($exception->getMessage());
+				$this->givePermissionsToPostTypes( [ 'faqs', 'vacancy' ], $editor );
+			} catch ( \TypeError $exception ) {
+				error_log( $exception->getMessage() );
 //				wp_die($exception->getMessage());
 			}
 		}
 
-		private function givePermissionsToPostTypes(array $postTypes, $role): void
-		{
-			if ($role instanceof \WP_Role) {
-			foreach ($postTypes as $postType) {
-				foreach ($this->create_post_type_permissions($postType) as $cap => $true):
-					$role->add_cap($cap);
-				endforeach;
-			}
+		private function givePermissionsToPostTypes( array $postTypes, $role ): void {
+			if ( $role instanceof \WP_Role ) {
+				foreach ( $postTypes as $postType ) {
+					foreach ( $this->create_post_type_permissions( $postType ) as $cap => $true ):
+						$role->add_cap( $cap );
+					endforeach;
+				}
 			}
 		}
 
